c3f33ae33ae734808f6e02b9ec825f5842694567
SerhiiChynin <sergey.chinin@gmail.com>
2022-12-13 10:05:39 +0200

    Routing React

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+
+
+function Header() {
+  return (
+      <>
+          <nav>
+              <li><a href="/">Main</a></li>
+              <li><a href="/about">About</a></li>
+              <li><a href="/users">Users</a></li>
+          </nav>
+      
+    </>
+  );
+}
+
+export default Header;
