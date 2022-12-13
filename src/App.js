f45ac77439b6dc391819c967ac7c7f9976aa2e30
SerhiiChynin <sergey.chinin@gmail.com>
2022-12-13 10:26:51 +0200

    BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 
 import './App.css';
 import Header from './Header';
+import Main from './Main'
+import About from './About';
+import Users from './Users';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Header/>
+      <Header />
+      <Router>
+        <Switch>
+          <Route path="/" component={Main} />
+          <Route path="about" component={About} />
+          <Route path="users" component={Users} />
+          
+        </Switch>
+      </Router>
+
+
+      <About/>
+      <Users/>
     </>
   );
 }
